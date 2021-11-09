5350143e4d442d0c654f6c5c7b961921d5525e41
NadiaNabila <nadianabilasalma@gmail.com>
2021-11-09 21:08:59 +0700

    Home Pantau Tanaman

---
+++ src/Components/Tabel_Plant.js
@@ -0,0 +1,14 @@
+import "./style/Tabel_Plant/Tabel_plant.css";
+
+function Tabel_Plant() {
+    return (
+  <div className="tabel-plant">
+      <div className="Title">
+         <h3 className="plant-Name">Sawi</h3>
+         <a className="plant-button">Pantau</a>
+     </div>
+</div>
+    );
+  }
+  
+  export default Tabel_Plant;
---
+++ src/Components/Tanamanku.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import Nav from "./Nav";
+import Footer from "./Footer";
+import Tabel_Plant from "./Tabel_Plant";
+
+const Tanamanku = () => {
+    return (
+      <div>
+        <Nav />
+        <Header />
+        <Footer />
+        <Tabel_Plant />
+      </div>
+    );
+  };
+  
+  export default Tanamanku;
+  
